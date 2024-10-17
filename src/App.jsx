147a69a886413db96506604492873c314fea4f4a
borz <117116989+borzX@users.noreply.github.com>
2024-10-17 16:13:47 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+import React from "react";
+import AddProduct from "./components/AddProduct";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div>
+      <h1>Product Catalog</h1>
+      <AddProduct />
+      <ProductList />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/EditProduct.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+
+
+const editProduct = () =>{
+    console.log('window')
+    return (
+        <form>
+            <input type="text" />
+        </form>
+    )
+}
+
+export default editProduct
